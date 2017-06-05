4a9ada63d93ec51ade8a593fcf098a830aa88707
David Lee <daviddwlee84@gmail.com>
2017-06-06 04:25:36 +0800

    Login System interact with database

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+/* MySQL */
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '1234',
+	database: 'ArtGallery'
+});
+connection.connect();
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { name: req.session.name });
 });
 
 router.post('/login', function(req, res, next) {
-    if (req.body.id === 'zzz' && req.body.pwd === '0000') {
-        req.session.name = 'david lee';
-        res.render('index', { name: req.session.name });
-    } else {
-        res.send('password error');
-    }
+
+	connection.query('SELECT ID AS id, Password AS pwd, Name FROM User WHERE ID = ? AND Password = ?',[req.body.id, req.body.pwd], function (err, result, fields) {
+		if (err) throw err;
+		console.log("userid = " + result[0].id + "userpwd = " + result[0].pwd);
+		console.log(result);
+		for(var i in result){
+			console.log(result[i]);
+		}
+    	if (req.body.id === result[0].id && req.body.pwd === result[0].id) {
+    	    req.session.name = result[0].Name;
+    	    res.render('index', { name: req.session.name });
+    	} else {
+    	    res.send('password error');
+    	}
+	});
+
+	// Bad writting style will cause SQL Injection
+	// connection.query(`SELECT ID, Password FROM User WHERE ID = ${req.body.id} AND Password = ${req.body.pwd}`, function (err, result, fields) {
+	// 	if (err) throw err;
+	// 	console.log(result);
+	// });
+
 });
 
 router.get('/page*', function(req, res, next) {
